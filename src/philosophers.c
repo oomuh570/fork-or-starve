8757c5b21218ed9574b995913b2f43d1e88d85fe
Dan Meulendyk <dmeul391@mtroyal.ca>
2026-03-21 11:08:58 +0000

    Minor documentation added

--- src/philosophers.c
+++ src/philosophers.c
@@ -24,13 +24,14 @@ int main()
 {
     pthread_t philosopher[NUM_PHILS];
     long i;
-    srand(time(0));
-
+    srand(time(0)); //Seeding random times for different run results
     sem_init(&mutex, 0, 1);
 
+    //Creating concurrent threads
     for (i = 0; i < NUM_PHILS; i++)
       pthread_create(philosopher + i, 0, think_and_eat, (void *)(long)i);
 
+    //Joining threads
     for (i = 0; i < NUM_PHILS; i++)
       pthread_join(philosopher[i], NULL);
     
@@ -43,12 +44,14 @@ void *think_and_eat(void *arg)     /* executed concurrently by all philosophers 
     long i = (long)arg;
 
     while (1){
+      
 	//Thinking
 	sem_wait(&mutex);
 	state[i] = THINKING;
 	printf("P%ld %s is THINKING\n", i, phil_names[i]);
 	sem_post(&mutex);
-	
+
+	//Sleeps for between 2-3 seconds
 	usleep((rand() % 1000 + 2000) * 1000);
 
 	//Hungry
@@ -56,8 +59,10 @@ void *think_and_eat(void *arg)     /* executed concurrently by all philosophers 
 	state[i] = HUNGRY;
 	printf("P%ld %s is HUNGRY\n", i, phil_names[i]);
 	sem_post(&mutex);
-	
+
+	//Sleeps for between 2-3 seconds
 	usleep((rand() % 1000 + 2000) * 1000);
+	
     }
 
     return 0;
