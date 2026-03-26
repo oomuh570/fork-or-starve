d37d6ff87c1f1bb702089e1406f9cf291bf2ffd1
Dan Meulendyk <dmeul391@mtroyal.ca>
2026-03-26 23:26:21 +0000

    Created deadlock by using usleep

--- src/philosophers.c
+++ src/philosophers.c
@@ -12,11 +12,15 @@ This code was taken from supplied lab material as a basic framework to develop o
 #define NUM_PHILS 5     
 #define THINKING 0
 #define HUNGRY 1
+#define EATING 2
 
+pthread_mutex_t forks[NUM_PHILS];
 int state[NUM_PHILS];
-char *phil_names[NUM_PHILS] = {"Aristotle", "Plato", "Scorates", "Descartes", "Kant"};
+char *phil_names[NUM_PHILS] = {"Aristotle", "Plato", "Socrates", "Descartes", "Kant"};
 
 void *think_and_eat(void *);
+int left_fork(int i);
+int right_fork(int i);
 
 sem_t mutex;
 
@@ -27,6 +31,11 @@ int main()
     srand(time(0)); //Seeding random times for different run results
     sem_init(&mutex, 0, 1);
 
+    //Creating mutexs for forks
+    for (i = 0; i < NUM_PHILS; i++){
+      pthread_mutex_init(&forks[i], NULL);
+    }
+    
     //Creating concurrent threads
     for (i = 0; i < NUM_PHILS; i++)
       pthread_create(philosopher + i, 0, think_and_eat, (void *)(long)i);
@@ -52,19 +61,48 @@ void *think_and_eat(void *arg)     /* executed concurrently by all philosophers 
 	sem_post(&mutex);
 
 	//Sleeps for between 2-3 seconds
-	usleep((rand() % 1000 + 2000) * 1000);
+	//	usleep((rand() % 1000 + 2000) * 1000);
 
+	usleep(10000);
+	
 	//Hungry
 	sem_wait(&mutex);
 	state[i] = HUNGRY;
 	printf("P%ld %s is HUNGRY\n", i, phil_names[i]);
 	sem_post(&mutex);
 
-	//Sleeps for between 2-3 seconds
-	usleep((rand() % 1000 + 2000) * 1000);
+	//Naive Fork Pickup (will cause deadlock)
+	pthread_mutex_lock(&forks[left_fork(i)]);
+	printf("P%ld %s picked up LEFT fork %d\n", i, phil_names[i], left_fork(i));
+
+	//Trying to create deadlock
+	usleep(50000);
 	
+	pthread_mutex_lock(&forks[right_fork(i)]);
+	printf("P%ld %s picked up RIGHT fork %d\n", i, phil_names[i], right_fork(i));
+
+	//EATING
+	sem_wait(&mutex);
+	state[i] = EATING;
+	printf("P%ld %s is EATING\n", i, phil_names[i]);
+	sem_post(&mutex);
+	
+	//Sleeps for between 2-3 seconds
+	//	usleep((rand() % 1000 + 2000) * 1000);
+	usleep(10000);
+
+	//Put forks down
+	pthread_mutex_unlock(&forks[right_fork(i)]);
+	pthread_mutex_unlock(&forks[left_fork(i)]);
     }
 
     return 0;
 }
 
+int left_fork(int i){
+  return i;
+}
+
+int right_fork(int i){
+  return (i + 1) % NUM_PHILS;
+}
