c825baca683c0293ed67e6490c8ca88a875e0bb2
Salman Tajammal <staja919@mtroyal.ca>
2026-04-09 23:20:54 -0600

    added documentation for fork files

--- src/forks.c
+++ src/forks.c
@@ -1,9 +1,28 @@
+/*
+ * File: forks.c
+ * Purpose: Gives the left and right fork index for a philosopher.
+ */
+
 #include "philosopher.h"
 
+/*
+ * Function: left_fork
+ * Purpose: Returns the left fork number for a philosopher.
+ * Params:
+ *   i - philosopher number
+ * Returns: left fork index
+ */
 int left_fork(int i){
   return i;
 }
 
+/*
+ * Function: right_fork
+ * Purpose: Returns the right fork number for a philosopher.
+ * Params:
+ *   i - philosopher number
+ * Returns: right fork index
+ */
 int right_fork(int i){
   return (i + 1) % NUM_PHILS;
 }
--- src/forks.h
+++ src/forks.h
@@ -1,7 +1,27 @@
+/*
+ * File: forks.h
+ * Purpose: Header file for fork helper functions.
+ */
+
 #ifndef FORKS_H
 #define FORKS_H
 
+/*
+ * Function: left_fork
+ * Purpose: Returns the left fork index for a philosopher.
+ * Params:
+ *   i - philosopher number
+ * Returns: left fork index
+ */
 int left_fork(int i);
+
+/*
+ * Function: right_fork
+ * Purpose: Returns the right fork index for a philosopher.
+ * Params:
+ *   i - philosopher number
+ * Returns: right fork index
+ */
 int right_fork(int i);
 
 #endif
