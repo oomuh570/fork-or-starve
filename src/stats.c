d4737063846ec9651d7f1d44e46d6fb48d828393
Salman Tajammal <staja919@mtroyal.ca>
2026-04-09 23:27:10 -0600

    added documentation for stats files

--- src/stats.c
+++ src/stats.c
@@ -1,3 +1,8 @@
+/*
+ * File: stats.c
+ * Purpose: Tracks meals eaten and checks for starvation.
+ */
+
 #include <stdio.h>
 #include "philosopher.h"
 
@@ -7,35 +12,49 @@
 #define DARK_RED "\033[38;5;88m"
 #define RESET "\033[0m"
 
+/*
+ * Function: meal_record
+ * Purpose: Adds one meal to a philosopher's total and prints it.
+ * Params:
+ *   i - philosopher number
+ * Returns: void
+ */
 void meal_record(long i) {
-	meals[i]++;
-	printf("%sP%ld %s have eaten %d meal(s)%s\n", YELLOW, i, phil_names[i], meals[i], RESET);
+    meals[i]++;
+    printf("%sP%ld %s have eaten %d meal(s)%s\n", YELLOW, i, phil_names[i], meals[i], RESET);
 }
 
+/*
+ * Function: starve_check
+ * Purpose: Checks meal counts to see if starvation may be happening.
+ * Params:
+ *   count - current step count used for checking starvation
+ * Returns: void
+ */
 void starve_check(int count) {
-	int min = meals[0];
-	int max = meals[0];
-	int min_index = 0;
+    int min = meals[0];
+    int max = meals[0];
+    int min_index = 0;
 
-	for (int i = 1; i < NUM_PHILS; i++) {
-		if(meals[i] < min) {
-			min = meals[i];
-			min_index = i;
-		}
-		if(meals[i] > max) {
-			max = meals[i];
-		}
-	}
+    for (int i = 1; i < NUM_PHILS; i++) {
+        if (meals[i] < min) {
+            min = meals[i];
+            min_index = i;
+        }
+        if (meals[i] > max) {
+            max = meals[i];
+        }
+    }
 
-	if (count >= 50 && min == 0) {
-		printf("%sFull starvation detected: P%d %s has 0 meals after %d steps%s\n", 
-			DARK_RED, min_index, phil_names[min_index], count, RESET);
-	}
-	else if (max - min > 3) {
-		printf("%sPotential starvation detected: P%d %s is lagging behind%s\n",
-			MAGENTA, min_index, phil_names[min_index], RESET);
-	}
-	else {
-		printf("%sFair distribution%s\n", TEAL, RESET);
-	}
+    if (count >= 50 && min == 0) {
+        printf("%sFull starvation detected: P%d %s has 0 meals after %d steps%s\n",
+            DARK_RED, min_index, phil_names[min_index], count, RESET);
+    }
+    else if (max - min > 3) {
+        printf("%sPotential starvation detected: P%d %s is lagging behind%s\n",
+            MAGENTA, min_index, phil_names[min_index], RESET);
+    }
+    else {
+        printf("%sFair distribution%s\n", TEAL, RESET);
+    }
 }
--- src/stats.h
+++ src/stats.h
@@ -1,7 +1,27 @@
+/*
+ * File: stats.h
+ * Purpose: Header file for meal tracking and starvation checking.
+ */
+
 #ifndef STATS_H
 #define STATS_H
 
+/*
+ * Function: meal_record
+ * Purpose: Adds one meal to a philosopher's total.
+ * Params:
+ *   i - philosopher number
+ * Returns: void
+ */
 void meal_record(long i);
+
+/*
+ * Function: starve_check
+ * Purpose: Checks if a philosopher may be starving.
+ * Params:
+ *   count - current step count
+ * Returns: void
+ */
 void starve_check(int count);
 
 #endif
