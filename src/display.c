648f951182958dfd878b2bdc7c5f25a9ee8cfaf2
Salman Tajammal <staja919@mtroyal.ca>
2026-04-09 23:14:38 -0600

    Added documentation for display files

--- src/display.c
+++ src/display.c
@@ -1,19 +1,32 @@
+/*
+ * File: display.c
+ * Purpose: Prints each philosopher's current state to the terminal.
+ */
+
 #include <stdio.h>
 #include "philosopher.h"
 
-#define GREEN "\033[92m"   //EATING
-#define RED "\033[91m"     //HUNGRY
-#define BLUE "\033[94m"    //THINKING
+#define GREEN "\033[92m"   // EATING
+#define RED "\033[91m"     // HUNGRY
+#define BLUE "\033[94m"    // THINKING
 #define RESET "\033[0m"
 
+/*
+ * Function: print_state
+ * Purpose: Prints one philosopher's current state with a color.
+ * Params:
+ *   i     - philosopher number
+ *   state - current state of the philosopher
+ * Returns: void
+ */
 void print_state(long i, int state) {
-	if (state == THINKING) {
-		printf("%sP%ld %s is THINKING%s\n", BLUE, i, phil_names[i], RESET);
-    	}
-	else if (state == HUNGRY) {
-		printf("%sP%ld %s is HUNGRY%s\n", RED, i, phil_names[i], RESET);
-    	}
-	else if (state == EATING) {
-		printf("%sP%ld %s is EATING%s\n", GREEN, i, phil_names[i], RESET);
-	}
+    if (state == THINKING) {
+        printf("%sP%ld %s is THINKING%s\n", BLUE, i, phil_names[i], RESET);
+    }
+    else if (state == HUNGRY) {
+        printf("%sP%ld %s is HUNGRY%s\n", RED, i, phil_names[i], RESET);
+    }
+    else if (state == EATING) {
+        printf("%sP%ld %s is EATING%s\n", GREEN, i, phil_names[i], RESET);
+    }
 }
--- src/display.h
+++ src/display.h
@@ -1,6 +1,19 @@
+/*
+ * File: display.h
+ * Purpose: Header file for display functions.
+ */
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+/*
+ * Function: print_state
+ * Purpose: Prints one philosopher's state to the terminal.
+ * Params:
+ *   i     - philosopher number
+ *   state - current state of the philosopher
+ * Returns: void
+ */
 void print_state(long i, int state);
 
 #endif
